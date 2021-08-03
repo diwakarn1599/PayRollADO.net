98e189a52979c350c41728b8640c82cb18fcd3f6
Diwakar <diwakar.n@tvsnext.io>
2021-08-03 16:40:43 +0530

    [Diwakar] Initial commit

---
+++ PayRollAdo.net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PayRollAdo.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PayRoll ADO .net");
+        }
+    }
+}
